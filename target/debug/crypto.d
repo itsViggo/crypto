7d277575da4bf94035ad288812f4c3585daa0a7f
itsViggo <vigo@192-168-1-113.tpgi.com.au>
2021-03-06 00:02:35 +1100

    initial commit

---
+++ target/debug/crypto.d
@@ -0,0 +1,1 @@
+/Users/vigo/Desktop/UNSW/COMP6841/SA/crypto/target/debug/crypto: /Users/vigo/Desktop/UNSW/COMP6841/SA/crypto/src/main.rs
---
+++ target/debug/deps/crypto.d
@@ -0,0 +1,5 @@
+/Users/vigo/Desktop/UNSW/COMP6841/SA/crypto/target/debug/deps/crypto: src/main.rs
+
+/Users/vigo/Desktop/UNSW/COMP6841/SA/crypto/target/debug/deps/crypto.d: src/main.rs
+
+src/main.rs:
